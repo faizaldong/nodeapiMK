6b98cfb55f380bbad695bcc946851de26a0c72f7
faizaldong <faizal.itech92@gmail.com>
2018-04-15 01:42:18 +0800

    change mysql pas

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "faizaL6275",
+  password: "faizaL6335",
   database: "majlis_kuliah"
 });
 
