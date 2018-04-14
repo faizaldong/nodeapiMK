a944d37e67dd791f5bf8fcfca52548445ec85c20
faizaldong <faizal.itech92@gmail.com>
2018-04-15 01:34:02 +0800

    debug data in controller

--- controllers/ajkController.js
+++ controllers/ajkController.js
@@ -2,6 +2,7 @@ var ajk_m = require('../models/ajkModel');
 
 exports.Listing = function(req, res) {
   ajk_m.GET(function(err, dataJson){
-    res.json({status: true, result: dataJson, admin: 'akmal', worker: 'faizal'})
+    res.json({status: true, result: dataJson, admin: 'akmal', worker: 'faizal'});
+    console.log(dataJson)
   })
 };
